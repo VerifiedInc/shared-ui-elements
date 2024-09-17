880c023af2ab0a9787966dde8403e86d8d2df769
Iagor Moraes <13892132+iagormoraes@users.noreply.github.com>
2024-09-17 10:23:03 -0300

    Revert "credential requests editor (#7)"
    This reverts commit 9e980527242446471ddeedd657c829cbd6954ee8.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,6 +3,6 @@ import type { ButtonProps as MUIButtonProps } from '@mui/material/Button';
 
 export type ButtonProps = MUIButtonProps;
 
-export function Button(props: ButtonProps): React.JSX.Element {
+export function Button(props: ButtonProps): JSX.Element {
   return <MUIButton {...props} />;
 }
