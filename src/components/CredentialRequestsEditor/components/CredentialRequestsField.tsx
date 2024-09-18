9767186cf966c33fba5e26eef7dde8e29a3d9c04
Iagor Moraes <13892132+iagormoraes@users.noreply.github.com>
2024-09-18 12:04:35 -0300

    fix: omit internal id prop from editor and parent removal on last child removal (#14)
    * fix: omit internal id prop from editor
    
    * fix: delete parent on last child credential request removal

--- src/components/CredentialRequestsEditor/components/CredentialRequestsField.tsx
+++ src/components/CredentialRequestsEditor/components/CredentialRequestsField.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Stack } from '@mui/material';
 import { Add } from '@mui/icons-material';
-import { useFieldArray, useFormContext } from 'react-hook-form';
+import {
+  useFieldArray,
+  type UseFieldArrayReturn,
+  useFormContext,
+} from 'react-hook-form';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
@@ -19,8 +23,15 @@ import {
 
 function CredentialRequestField({
   path = 'credentialRequests',
+  parentFieldArray,
+  parentIndex = 0,
   level = 0,
-}: Readonly<{ path?: string; level?: number }>): React.JSX.Element {
+}: Readonly<{
+  path?: string;
+  parentFieldArray?: UseFieldArrayReturn<CredentialRequestsEditorForm>;
+  parentIndex?: number;
+  level?: number;
+}>): React.JSX.Element {
   const customConfig = useCredentialRequestsEditor();
   const form = useFormContext<CredentialRequestsEditorForm>();
   const fieldArray = useFieldArray<CredentialRequestsEditorForm>({
@@ -40,12 +51,17 @@ function CredentialRequestField({
             fieldArray={fieldArray}
             index={index}
             level={level}
+            onAllFieldsDelete={() => {
+              (parentFieldArray ?? fieldArray)?.remove(parentIndex);
+            }}
           >
             <DataFieldAccordion />
             {Array.isArray(field.children) && (
               <CredentialRequestField
                 key={`${_path}.children`}
                 path={`${_path}.children`}
+                parentFieldArray={fieldArray}
+                parentIndex={index}
                 level={level + 1}
               />
             )}
