cb2c3ce567024b4488c20f58e5ffaf3b78896b63
Gabriel Borges <gabriel@verified.inc>
2024-10-30 16:03:54 -0300

    fix:  type issues

--- src/stories/components/form/DateInput.stories.ts
+++ src/stories/components/form/DateInput.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { DateInput } from '../../../components/form/DateInput';
 import { fn } from '@storybook/test';
+import { DateInput } from '../../../components/form/DateInput';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
