086c6271c81901386b4772780c1dce9a44da12c3
Gabriel Borges <gabriel@verified.inc>
2024-11-06 11:31:48 -0300

    chore: setup bundler

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite';
 import { configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react({ jsxRuntime: 'automatic' })],
+  plugins: [react({ jsxRuntime: 'automatic' }), dts({ include: ['src'] })],
   test: {
     // 👋 add the line below to add jsdom to vite
     environment: 'jsdom',
     exclude: [...configDefaults.exclude, '.trunk', '.storybook'],
+  },
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+    },
   },
 });
