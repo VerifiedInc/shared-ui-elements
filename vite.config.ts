ac3b3bafbf2de64715387bde049a30b3e5a28dc0
gabriel-verified <gabriel@verified.inc>
2024-11-06 11:48:32 -0300

    Chore Bundler and prerelease script (#49)
    * chore: add peer deps and prerelease
    
    * 0.10.1-beta.0
    
    * chore: setup bundler
    
    * 0.10.1-beta.0
    
    * 0.10.1-beta.2
    
    * chore: add external packages to rollup config
    
    * 0.10.1-beta.3
    
    * 0.10.1-beta.4
    
    * feat: new minor version with the bundler working

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,40 @@
 import { defineConfig } from 'vite';
 import { configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react({ jsxRuntime: 'automatic' })],
+  plugins: [react({ jsxRuntime: 'automatic' }), dts({ include: ['src'] })],
   test: {
-    // 👋 add the line below to add jsdom to vite
     environment: 'jsdom',
     exclude: [...configDefaults.exclude, '.trunk', '.storybook'],
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: [
+        '@emotion/react',
+        '@emotion/styled',
+        '@fontsource/lato',
+        '@fontsource/material-icons',
+        '@mona-health/react-input-mask',
+        '@mui/icons-material',
+        '@mui/material',
+        'jsdom',
+        'libphonenumber-js',
+        'qrcode',
+        'react',
+        'react-dnd',
+        'react-dnd-html5-backend',
+        'react-dom',
+        'react-hook-form',
+        'react-imask',
+        'zod',
+      ],
+    },
   },
 });
