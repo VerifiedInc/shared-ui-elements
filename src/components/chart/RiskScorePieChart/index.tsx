d451881d498d9f4c482ba3de0b04a1339d585540
Iagor Moraes <iagor2014@outlook.com>
2025-02-24 18:41:43 -0300

    refactor: remove unused code

--- src/components/chart/RiskScorePieChart/index.tsx
+++ src/components/chart/RiskScorePieChart/index.tsx
@@ -1,7 +1,7 @@
 import { type ReactElement } from 'react';
 import { useTheme, type SxProps } from '@mui/material';
 
-import { PieChart, type DataPoint } from '../PieChart';
+import { PieChart } from '../PieChart';
 import { EmptyChartSection } from '../EmptyChartSection';
 import { LoadingChartSection } from '../LoadingChartSection';
 import { useStyle } from '../styles';
