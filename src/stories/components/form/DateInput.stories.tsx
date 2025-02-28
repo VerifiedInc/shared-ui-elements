3dfa87540c5be4c1ea95463d21f3b85059d7f61d
Iagor Moraes <iagor2014@outlook.com>
2025-02-28 08:50:24 -0300

    fix(DateInput): Added  new prop to vertical align picker preventing overflow

--- src/stories/components/form/DateInput.stories.tsx
+++ src/stories/components/form/DateInput.stories.tsx
@@ -1,20 +1,28 @@
+import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
+import { Box, Stack } from '@mui/material';
+
 import { DateInput } from '../../../components/form/DateInput';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Components/form/DateInput',
   component: DateInput,
+  render: (args: any) => (
+    <Stack
+      justifyContent='center'
+      alignItems='center'
+      sx={{ width: 500, height: 400 }}
+    >
+      <Box>
+        <DateInput {...args} />
+      </Box>
+    </Stack>
+  ),
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
-    docs: {
-      story: {
-        width: '12600px',
-        height: '400px',
-      },
-    },
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
@@ -37,6 +45,20 @@ export const Default: Story = {
     error: false,
     size: 'small',
     helperText: 'Helper text',
+  },
+  argTypes: {},
+};
+
+export const PickerOverflow: Story = {
+  args: {
+    name: 'date',
+    label: 'Label',
+    onChange: fn(),
+    disabled: false,
+    error: false,
+    size: 'small',
+    helperText: 'Helper text',
+    overflowPicker: true,
   },
   argTypes: {},
 };
