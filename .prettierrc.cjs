8b67ab6afea599753440363572128a2a2eea6bcb
Iagor Moraes <iagor2014@outlook.com>
2025-03-07 11:28:37 -0300

    chore: trunk fmt

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,4 +1,7 @@
 module.exports = {
   singleQuote: true,
   jsxSingleQuote: true,
+  // Maintaining parentheses from Trunk formatting
+  // This is important for compatibility with Trunk linting
+  arrowParens: 'always',
 };
