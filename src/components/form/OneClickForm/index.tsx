2c8783e9d1681ae8b3289b6d37220e17ed254826
Iagor Moraes <iagor2014@outlook.com>
2025-03-26 09:45:39 -0300

    fix: export form utils

--- src/components/form/OneClickForm/index.tsx
+++ src/components/form/OneClickForm/index.tsx
@@ -14,6 +14,7 @@ import {
 } from './types/request';
 
 export * from './components/CredentialsDisplay/types';
+export * from './components/CredentialsDisplay/utils';
 
 type OneClickFormProps = {
   credentialRequests?: CredentialRequests[];
