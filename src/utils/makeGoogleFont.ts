00a1e790520655ca26db7f22ffa7b528591ac17b
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2025-04-24 18:46:51 -0300

    Add utility to generate Google Fonts URL with configurable font family and weights

---
+++ src/utils/makeGoogleFont.ts
@@ -0,0 +1,12 @@
+export const DEFAULT_FONT_FAMILY = 'Lato';
+export const DEFAULT_FONT_WEIGHTS = [300, 400, 500, 700, 900];
+
+export const makeGoogleFontUrl = (family: string | undefined) => {
+  const url = new URL('https://fonts.googleapis.com/css2');
+  url.searchParams.set(
+    'family',
+    `${family || DEFAULT_FONT_FAMILY}:wght@${DEFAULT_FONT_WEIGHTS.join(';')}`,
+  );
+  url.searchParams.set('display', 'swap');
+  return url.toString();
+};
