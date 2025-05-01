5dfbcda50fb253514d99fdc01029932cf228687f
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2025-05-01 14:09:24 -0300

    fix timestamp range to give utc timestamps

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -69,3 +69,15 @@ export const formatExtendedDate = (
     ...options,
   });
 };
+
+export const toUTCMilliseconds = (date: Date): number => {
+  return Date.UTC(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds(),
+  );
+};
