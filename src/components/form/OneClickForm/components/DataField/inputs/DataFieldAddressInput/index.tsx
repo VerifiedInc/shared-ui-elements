d1b9f5ab6b7d6e8346a4223bc2bc62cff49925cd
matheusverifiedinc <matheus@verified.inc>
2025-05-13 19:34:16 -0300

    Fix package lock (#243)
    ## Summary
    Fix package lock
    
    ## Changes
    Fix package lock
    Fix MUI imports
    
    ## Testing
    Tested locally
    
    ## Checklist
    
    - [x] I have performed a self-review of my code.
    - [ ] I have commented my code, particularly in hard-to-understand areas
    _to a borderline excessive amount_
    - [ ] I have made any relevant changes to the documentation, including
    the project readme.
    - [x] I have run and tested the changes locally
    - [ ] If it is a core feature, I have added appropriate unit tests.
    - [ ] Any dependent changes have been merged and published in upstream
    projects.

--- src/components/form/OneClickForm/components/DataField/inputs/DataFieldAddressInput/index.tsx
+++ src/components/form/OneClickForm/components/DataField/inputs/DataFieldAddressInput/index.tsx
@@ -9,7 +9,7 @@ import {
   Typography,
   useTheme,
 } from '@mui/material';
-import Grid2 from '@mui/material/Unstable_Grid2';
+import Grid2 from '@mui/material/Grid2';
 
 import { inputStyle } from '../../../../styles/input';
 
