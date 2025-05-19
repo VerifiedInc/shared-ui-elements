27474d5f43bdcef64fc764e58f967ffd4ae5c546
Iagor Moraes <iagor2014@outlook.com>
2025-05-19 16:18:28 -0300

    refactor: use component type

--- src/components/animation/motions.ts
+++ src/components/animation/motions.ts
@@ -9,30 +9,30 @@ import {
 } from '@mui/material';
 import { motion } from 'framer-motion';
 
-export const MotionBox = motion(Box as any, {
+export const MotionBox = motion(Box as React.ComponentType<any>, {
   forwardMotionProps: false,
 });
 
-export const MotionStack = motion(Stack as any, {
+export const MotionStack = motion(Stack as React.ComponentType<any>, {
   forwardMotionProps: false,
 });
 
-export const MotionTbody = motion(TableBody as any, {
+export const MotionTbody = motion(TableBody as React.ComponentType<any>, {
   forwardMotionProps: false,
 });
 
-export const MotionTableRow = motion(TableRow as any, {
+export const MotionTableRow = motion(TableRow as React.ComponentType<any>, {
   forwardMotionProps: false,
 });
 
-export const MotionTableCell = motion(TableCell as any, {
+export const MotionTableCell = motion(TableCell as React.ComponentType<any>, {
   forwardMotionProps: false,
 });
 
-export const MotionTypography = motion(Typography as any, {
+export const MotionTypography = motion(Typography as React.ComponentType<any>, {
   forwardMotionProps: false,
 });
 
-export const MotionButton = motion(Button as any, {
+export const MotionButton = motion(Button as React.ComponentType<any>, {
   forwardMotionProps: false,
 });
