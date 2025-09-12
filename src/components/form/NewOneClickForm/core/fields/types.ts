1bb073386d01eab2fe005bbd572eb65b71952778
Iagor Moraes <iagor2014@outlook.com>
2025-09-12 14:31:43 -0300

    refactor: remove unused types

--- src/components/form/NewOneClickForm/core/fields/types.ts
+++ src/components/form/NewOneClickForm/core/fields/types.ts
@@ -1,5 +1,4 @@
 import { z } from 'zod';
-import type { FieldSchemaDefinitions } from '../declarations';
 
 // Base field characteristics contracts
 export interface TextFieldCharacteristics {
@@ -57,16 +56,3 @@ export interface CompositeFieldDefinition<TKey extends string>
 
 export interface DateFieldDefinition<TKey extends string>
   extends BaseFieldDefinition<TKey, DateFieldCharacteristics> {}
-
-// Extract field keys for type safety
-export type FieldKey = keyof FieldSchemaDefinitions;
-
-// Extract credential types for type safety (now same as field keys)
-export type CredentialType = FieldSchemaDefinitions[FieldKey]['key'];
-
-// Helper type to get field definition by key
-export type GetFieldDefinition<K extends FieldKey> = FieldSchemaDefinitions[K];
-
-// Helper type to get credential type by field key (now same as key)
-export type GetCredentialType<K extends FieldKey> =
-  FieldSchemaDefinitions[K]['key'];
